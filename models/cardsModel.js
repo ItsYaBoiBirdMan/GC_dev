02e6a491184a4697bff1cddde65515a7584c2449
ItsYaBoiBirdMan <94912025+ItsYaBoiBirdMan@users.noreply.github.com>
2022-04-24 16:55:00 +0100

    Customized the buttons

--- models/cardsModel.js
+++ models/cardsModel.js
@@ -2,7 +2,7 @@ var pool = require('./connection.js')
 
 module.exports.getAllCards = async function () {
     try {
-      let sql = `Select * from card`;
+      let sql = `Select * from card order by card_id asc`;
       let result = await pool.query(sql);
       let cards = result.rows;
       return { status: 200, result: cards };
